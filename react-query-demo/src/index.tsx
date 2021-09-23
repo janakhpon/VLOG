b9ac0e06943513ef9a99693af2f6321b3df8f374
janakhpon <jnovaxer@gmail.com>
2021-09-23 23:26:16 +0630

    [updates] - added a new article about react-query

---
+++ react-query-demo/src/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools'
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: true,
+    },
+  },
+})
+
+ReactDOM.render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+      {/* Enabling ReactQueryDevTools  */}
+      <ReactQueryDevtools initialIsOpen={false} /> 
+    </QueryClientProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
---
+++ react-query-demo/src/utils/task.common.ts
@@ -0,0 +1,21 @@
+export const URL = `http://localhost:3001/api/v1/todos`
+
+const isFloat = (n: number) => {
+    return Number(n) === n && n % 1 !== 0;
+}
+
+export const calculatePagination = (count: number) => {
+    let condition = 0
+    if (count !== 0) {
+        if (isFloat(count / 5)) {
+            let x = count / 5;
+            let decimals = x - Math.floor(x);
+            condition = x - decimals + 1;
+        } else {
+            condition = count / 5;
+        }
+    }
+    return condition
+}
+
+export const days = ["Sunday", "Monday", "Tuesday", "Wednesday ", "Thursday", "Friday", "Saturday"]
